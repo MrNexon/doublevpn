558f7fac95ce34c4ccc9b14147d47215b5c0d2d3
Efim Belyakov <30877121+MrNexon@users.noreply.github.com>
2024-02-13 15:34:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ else
     echo "Ansible already installed"
 fi
 
-git clone https://github.com/it-toppp/doublevpn.git && cd /root/doublevpn/
+git clone https://github.com/MrNexon/doublevpn.git && cd /root/doublevpn/
 
 ansible-playbook gen_conf.yml
 echo "Please wait..."
